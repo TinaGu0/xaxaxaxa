d4079b86544d7af9fd12fdb6211535556481e285
TinaGu0 <tinaguo0904@gmail.com>
2024-03-04 23:50:17 -0500

    yay

---
+++ a/src/components/CartArray.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="item">
+        <p>{{ item.name }}</p>
+        <button @click="remove">Remove</button>
+    </div>
+</template>
+
+<script>
+import { store } from '../views/store.js'
+
+export default {
+    name: "CartArray",
+    props: {
+        item: Object,
+        cart: Array,
+    },
+    methods: {
+        remove() {
+            store.cart.splice(this.index, 1);
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.item {
+    background-color: #FFF;
+    margin-bottom: 2vw;
+}
+
+</style>
